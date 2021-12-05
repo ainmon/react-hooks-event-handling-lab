d9d813684a91c6a7bc97777ce5d363d8278972a1
Adam <inmon.adam@yahoo.com>
2021-12-05 12:51:47 -0600

    commit tree 62ee1fdab35a2f1a6f1412501f4092eb92efa8b4

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+    function focusHandle(){
+        console.log('Good!')
+    }
+
+    function blurHandle(){
+        console.log('Hey! Eyes on me!')
+    }
+
+    return (
+        <button onFocus={focusHandle} onBlur={blurHandle}>Eyes on me </button>
+   )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+
+function Keypad() {
+    function handleChange(){
+        console.log('Entering password...')
+    }
+
+    return (
+        <input type='password' name='password' onChange={handleChange} >
+        </input>
+    )
+}
+
+export default Keypad
